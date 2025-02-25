dacbf8ba2c10800c07d530d5b17c772a1df6a00d
LeoDoCarmo <202401237906@alunos.estacio.br>
2025-02-25 20:05:42 +0000

    ajustando entrada e saida parte 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,45 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void limpar_entrada() {
+ char c;
+ while ((c = getchar()) != '\n' && c != EOF) {}
+}
+void ler_texto(char *buffer, int length) {
+ fgets(buffer, length, stdin);
+ strtok(buffer, "\n");
+}
 
 int main() {
-    char Estado1, Estado2; 
-    char[] CodigoDaCarta1, CodigoDaCarta2, NomeDaCidade1, NomeDaCidade2; 
-    int populacao1, populacao2,  NumeroPontoTuristico1, NumeroPontoTuristico2; 
-    float area1, area2, PIB1, PIB2;
+    char Estado1;
+    char CodigoDaCarta1[50], NomeDaCidade1[50];
+    int populacao1, NumeroPontoTuristico1;
+    float area1, PIB1;
 
-    printf("Digite os dados das cartas: \n\n");
-    printf("Carta 1\n");
+    printf("Digite os dados das cartas: \n");
+    printf("Carta 1 \n");
+
     printf("Estado: ");
-    scanf("%c", &Estado1);
-    printf("\n");
-    printf("Código: ");
-    scanf("%S", &CodigoDaCarta1);
-    printf("\n");
+    scanf("c", &Estado1);
+
+    printf("Codigo: ");
+    limpar_entrada();
+    scanf("%S", &CodigoDaCarta1[50]);
+
     printf("Nome da Cidade: ");
+    limpar_entrada();
     scanf("%s", &NomeDaCidade1);
-    printf("\n");
-    printf("População: ");
+
+    printf("Populacao: ");
     scanf("%d", &populacao1);
-    printf("\n");
-    printf("Área: ");
+
+    printf("Area: ");
     scanf("%f", &area1);
-    printf("\n");
+
     printf("PIB: ");
     scanf("%f", &PIB1);
-    printf("\n");
-    printf("Número de Pontos Turísticos: ");
+
+    printf("Numero de Pontos Turisticos: ");
     scanf("%d", &NumeroPontoTuristico1);
     printf("\n");
 
+    limpar_entrada();
 
-    printf("Carta 1\n");
+    printf("Carta 1 \n");
     printf("Estado: %c\n", Estado1);
-    printf("Código: %s\n", CodigoDaCarta1);
+    printf("Codigo: %s\n", CodigoDaCarta1);
     printf("Nome da Cidade: %s\n", NomeDaCidade1);
-    printf("População: %d\n", populacao1);
-    printf("Área: %f\n", area1);
-    printf("PIB: %f\n", PIB1);
-    printf("Número de Pontos Turísticos: %f\n", NumeroPontoTuristico1);
-    
-    
-    return 0;
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %.2f\n", area1);
+    printf("PIB: %.2f\n", PIB1);
+    printf("Numero de Pontos Turisticos: %d \n", NumeroPontoTuristico1);
+
+return 0;
 }
